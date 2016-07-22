4fff6f353e8648dbebd37250f55b549ff56ceed4
Oliver snow <oliver.d.snow@gmail.com>
2016-07-22 09:34:46 -0700

    removed old version of function

--- fitch_score_nj.py
+++ fitch_score_nj.py
@@ -91,84 +91,6 @@ def score_change(tree):
 				child.name = change
 	return tree
 
-def FitchScore_snow1(treefile, topofile):#{{{
-	"""
-	Snow's old implementation
-	"""
-	tree = Phylo.read(treefile, 'newick') #read from file into tree object
-#make dictionary of protein IDs and their topologies from topology file using SeqIO
-	topos = {}
-	for seq_record in SeqIO.parse(topofile, "fasta"):
-		if GetNtermState(seq_record.seq) == 'i':
-			nterm = '-'
-		else:
-			nterm = '+'
-		topos[seq_record.id[3:9]] = nterm + str(CountTM(str(seq_record.seq)))
-
-	count = 0
-	nodes = tree.get_nonterminals(order="postorder")#Gets internal nodes in a list going up the tree
-	for clade in nodes: 
-		sub_branches = clade.clades #the children of each internal node
-		print "sub_branches=",sub_branches
-		state1 = [] # I want these as lists so I can iterate through them 
-		state2 = []
-		print "state1=", state1
-		print "state2=", state2
-		if type(sub_branches[0].name) == str: # This block is pretty ugly but I couldn't find a 
-			state1.append(sub_branches[0].name) # better way of dealing with the single strings at the beginning
-		if type(sub_branches[1].name) == str:
-			state2.append(sub_branches[1].name)
-		if type(sub_branches[0].name) == list:
-			state1 = sub_branches[0].name
-		if type(sub_branches[1].name) == list:
-			state2 = sub_branches[1].name
-
-		clade.name = []
-		match = 0
-		"""iterate through each item the child nodes and check if their topologies are the same;
-		if there are no matching pairs, then iterate the count and label the next node with those names"""
-		for n in state1: 
-			for i in state2:
-				if topos[n] == topos[i]:
-					clade.name = [n]
-					match +=1
-
-		if match == 0:
-# 		print "clade.name before extend=", clade.name
-			clade.name.extend(state1)
-			clade.name.extend(state2)
-# 		print "clade.name after extend=", clade.name
-			print
-			count+=1
-# 	else:
-# 	    print "clade.name with match=", clade.name
-		
-	print "The total number of mutation events is:", count # count is equal to the number of mutations that have happened 
-
-	'''Iterate throught the nodes in the tree and remove child nodes if they are same topology as parent so that 
-	it leaves only the changes'''
-	for clade in nodes:
-		if type(clade.name) == list:
-			clade.name = ','.join(clade.name) 
-		for child in clade.clades:
-			if child.name in topos and clade.name in topos:
-				if topos[child.name] == topos[clade.name]:
-					try:
-						tree.prune(target=child)
-					except ValueError:
-						pass
-
-		# This renames the internal nodes that are left with their topologies
-		clade.name = ', '.join([topos[n] for n in clade.name.split(',')]) 
-
-# This renames the terminals with their topologies 
-	for term in tree.get_terminals():
-		term.name = topos[term.name]
-					
-	tree.ladderize()
-	Phylo.draw(tree, show_confidence=False) # can be changed to draw_ascii and also to show_confidence=True
-
-#}}}
 
 def FitchScore_nj1(treefile, topofile):#{{{
 	"""
@@ -202,10 +124,10 @@ def FitchScore_nj1(treefile, topofile):#{{{
 
 	for clade in nodes: 
 		sub_branches = clade.clades #the children of each internal node
-		state1 = [] # I want these as lists so I can iterate through them 
+		state1 = []  
 		state2 = []
-		if type(sub_branches[0].name) == str: # This block is pretty ugly but I couldn't find a 
-			state1.append(sub_branches[0].name) # better way of dealing with the single strings at the beginning
+		if type(sub_branches[0].name) == str: 
+			state1.append(sub_branches[0].name) 
 		if type(sub_branches[1].name) == str:
 			state2.append(sub_branches[1].name)
 		if type(sub_branches[0].name) == list:
